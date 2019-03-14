9766e8007c6cbb7936738fe418e2058d6c8af99c
chenhao920815 <48143711+chenhao920815@users.noreply.github.com>
2019-03-14 21:47:38 +0900

    社員管理システム
    社員管理システム　

---
+++ Employee.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheyuan;
+
+/**
+ *
+ * @author user
+ */
+public class Employee {
+
+    private String ID;
+    private String NAME;
+    private String SEX;
+    private String POS;
+    private String ENTRYDATE;
+
+    /**
+     * @return the ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the NAME
+     */
+    public String getNAME() {
+        return NAME;
+    }
+
+    /**
+     * @param NAME the NAME to set
+     */
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    /**
+     * @return the SEX
+     */
+    public String getSEX() {
+        return SEX;
+    }
+
+    /**
+     * @param SEX the SEX to set
+     */
+    public void setSEX(String SEX) {
+        this.SEX = SEX;
+    }
+
+    /**
+     * @return the POS
+     */
+    public String getPOS() {
+        return POS;
+    }
+
+    /**
+     * @param POS the POS to set
+     */
+    public void setPOS(String POS) {
+        this.POS = POS;
+    }
+
+    /**
+     * @return the ENTRYDATE
+     */
+    public String getENTRYDATE() {
+        return ENTRYDATE;
+    }
+
+    /**
+     * @param ENTRYDATE the ENTRYDATE to set
+     */
+    public void setENTRYDATE(String ENTRYDATE) {
+        this.ENTRYDATE = ENTRYDATE;
+    }
+
+}
---
+++ EmployeeService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheyuan;
+
+import java.awt.List;
+
+/**
+ *
+ * @author user
+ */
+public class EmployeeService {
+
+    public boolean createEmployeeData(String eName, String eSex, String ePos) {
+        return false;
+    }
+
+}
